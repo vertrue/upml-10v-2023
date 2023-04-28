79f24e378b4e92c45ef7d95cdc3edb918d0f184f
arsen1355 <127500320+arsen1355@users.noreply.github.com>
2023-04-28 13:06:34 +0300

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 
 using namespace std;
@@ -9,7 +11,7 @@ int main()
     cin >> n;
     
     // створення масиву
-    int a[n]
+    int a[n];
     for(int i = 0; i < n; i++)
     {
         cin >> a[i];
@@ -17,18 +19,18 @@ int main()
     
     // чи є хоча б 3 нулі
     int zeros = 0;
-    for(int i = 1; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        if(a[i] = 0)
+        if(a[i] == 0)
         {
-            zeros += 1
+            zeros += 1;
         }
     }
     
     // виведення
-    if(zeros > 3)
+    if(zeros >= 3)
         cout << "є" << endl;
     else
         cout << "нема" << endl;
+    return 0;
 }
-
